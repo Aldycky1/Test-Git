625caa9a100790e176df3feceb37de836f35485b
Aldycky1 <bagusaldycky@gmail.com>
2021-06-11 14:54:26 +0700

    First commit

---
+++ case-sensitive.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 70;
+$b = 80;
+$c = $a + $b;
+$result = 0;
+
+
+// $result += $c;
+// echo $result; //150
+
+$result = $result + $c;
+echo $result;
---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+
+$numbers = array(1, 2, 3, 4, 5);
+
+foreach ($numbers as $value) {
+    echo "Value is $value <br/>";
+}
+
+var_dump($numbers);
